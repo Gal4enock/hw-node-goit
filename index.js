03148de5996ee5795383aa80f2f12c638aa5a67e
Galyna Khyzhynska <gal4enock86@gmail.com>
2021-02-05 14:45:31 +0200

    добавляет базовые настройки

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 const argv = require('yargs').argv;
+const cors = require('cors');
+const express = require('express');
 
-// import some from 'yargs';
-// argv = some.argv;
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res, next) => {
+  res.json({message: 'CORS is activated'})
+})
+
+app.listen(PORT, () => {
+  console.log('Listening with CORS on port..', PORT);
+})
 
 const { listContacts, getContactById, removeContact, addContact } = require ('./contacts.js');
-// import { listContacts, getContactById, removeContact, addContact } from './contacts.js';
 
+// function invokeAction({ action, id, name, email, phone }) {
+//   switch (action) {
+//     case 'list':
+//       listContacts()
+//       break;
 
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case 'list':
-      listContacts()
-      break;
+//     case 'get':
+//       // contacts.getContactById(id)
+//       getContactById(id)
+//       break;
 
-    case 'get':
-      // contacts.getContactById(id)
-      getContactById(id)
-      break;
+//     case 'add':
+//       // contacts.addContact(name, email, phone)
+//       addContact(name, email, phone)
+//       break;
 
-    case 'add':
-      // contacts.addContact(name, email, phone)
-      addContact(name, email, phone)
-      break;
+//     case 'remove':
+//       removeContact(id)
+//       break;
 
-    case 'remove':
-      removeContact(id)
-      break;
+//     default:
+//       console.warn('\x1B[31m Unknown action type!');
+//   }
+// }
 
-    default:
-      console.warn('\x1B[31m Unknown action type!');
-  }
-}
+// invokeAction(argv);
 
-invokeAction(argv);
