2ddb0b321b9de5bd4f57514e80f939d7e6753ae4
Galyna Khyzhynska <gal4enock86@gmail.com>
2021-02-12 18:43:13 +0200

    добавляет схему Юзера

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+const UserSchema = new Schema({
+  email: String,
+  password: String,
+  subscription: {
+    type: String,
+    enum: ["free", "pro", "premium"],
+    default: "free"
+  },
+  token: String
+})
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User
